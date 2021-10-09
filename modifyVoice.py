9bdd01db0febf60ccf8da3febb0509e89d2f6718
Drelf2018 <41439182+Drelf2018@users.noreply.github.com>
2021-10-09 19:30:37 +0800

    update voices

--- modifyVoice.py
+++ modifyVoice.py
@@ -7,9 +7,21 @@ def match_target_amplitude(sound, target_dBFS):
     return sound.apply_gain(change_in_dBFS)
 
 
-for root, folders, files in os.walk('./public/voices'):
-    for file in files:
-        sound = pydub.AudioSegment.from_file(os.path.join(root, file), "mp3")
-        print(os.path.join(root, file), sound.dBFS)
-        # normalized_sound = match_target_amplitude(sound, -25.0)
-        # normalized_sound.export(os.path.join(root, file), format="mp3")
+def modifyAll():
+    for root, folders, files in os.walk('./public/voices'):
+        for file in files:
+            sound = pydub.AudioSegment.from_file(os.path.join(root, file), "mp3")
+            normalized_sound = match_target_amplitude(sound, -25.0)
+            normalized_sound.export(os.path.join(root, file), format="mp3")
+
+
+def view():
+    for root, folders, files in os.walk('./public/voices'):
+        for file in files:
+            sound = pydub.AudioSegment.from_file(os.path.join(root, file), "mp3")
+            print(os.path.join(root, file), sound.dBFS)
+
+
+modifyAll()
+view()
+input()
